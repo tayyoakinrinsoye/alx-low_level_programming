1ab116eee807b66ee1cbb51609ceec74d1c7e3d7
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-07 10:15:43 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - prints the phrase -with proper grammar, but the outcome is a piece of art,
+ * Return: 0 if exited the right way, otherwise returns non-zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
