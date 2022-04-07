4e6b04ace109b8af5ba54b388830f1d1dcf121eb
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-07 09:57:23 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- *main - prints the statement "Programming is like building a multilingual puzzle
+ *main - prints the phrase "Programming is like building a multilingual puzzle
  *return 0 if exited properly and non-zero otherwise
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
+	return (0);
 }
