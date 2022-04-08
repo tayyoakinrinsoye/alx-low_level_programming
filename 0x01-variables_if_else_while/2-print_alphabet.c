6e4c8178dd27a869d79ed35a1a1bf519dd939c76
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-08 16:05:20 -0700

    Alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints the alphabet in lower case
+ * Return: 0
+ */ 
+ int main(void)
+ {
+	char k = 'a';
+	while (k <= 'z')
+	{
+		putchar(k);
+		k++;
+	}
+	putchar('\n');
+	return (0);
+ }
