67949b26bda34dfc9533d3ed2b98c33c4d3ce861
Tayo Akinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-11 13:09:55 -0700

    101-printcomb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,27 +9,22 @@ int main(void)
 {
 	int i, j, k;
 
-		for (i = 0; i <= 9; i++)
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
 		{
-			for (j = 0; j <= 9; j++)
+			for (k = j + 1; k < 10; k++)
 			{
-				for (k = 0; k <= 9; k++)
-				{
-					if (k != j && j != i && i < j && j < k)
-					{
-						putchar(48 + i);
-						putchar(48 + k);
-						putchar(48 + j);
-						if (i == 7 && j == 8 && k == 9)
-						{
-						break;
-						}
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
+				if (i == 7 && j == 8 && k == 9)
+				continue;
+				putchar(',');
+				putchar(' ');	
 			}
 		}
-		putchar('\n');
-		return (0);
+	}
+	putchar('\n');
+	return (0);
 }
