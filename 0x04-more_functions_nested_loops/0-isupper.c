263e23e3349c7808d00aead6466cb72e8d4fadeb
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-14 23:34:03 +0000

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper: - Program that checks for uppercase character
+ * _isupper - Program that checks for uppercase character
  *  and returns 1 if c is uppercase and zero otherwise
  * @c: character to be tested
  * Return: 1 fi c is uppercase and 0 otherwise
