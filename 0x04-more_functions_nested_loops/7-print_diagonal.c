ee9513cc1011295cb04c9a62eba351a6ed2c9335
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-14 21:04:15 +0000

    Task 6

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagonal - function that draws a daigonal line on the terminal
+ *
+ * @n: number of times the line will be drawn
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		print('\n');
+	}
+	else
+	{
+		for (i = 1
+	}
+}
