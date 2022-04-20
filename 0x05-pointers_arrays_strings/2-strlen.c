62b185e797949ffac340cdd6f9f7069f34b184ec
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-20 16:28:07 +0000

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 /**
  * _strlen - function returns the length of a string
  * @s:the string to be passed
- * Return: 0 always success
+ * Return: length of a string
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (*s[i] != '\0')
 	{
 		i++;
 		s++;
