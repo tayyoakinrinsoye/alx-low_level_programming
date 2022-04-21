80e9e6370b95b6fc61e263c6dbb359b40df44eec
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-21 05:00:49 +0000

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int k;
 
-	for (k = 0; src[k] != '\0'; j++)
+	for (k = 0; src[k] != '\0'; k++)
 	{
-		dest[j] = src[j];
+		dest[k] = src[k];
 	}
-	dest[j] = '\0';
+	dest[k] = '\0';
 	return (dest);
 }
