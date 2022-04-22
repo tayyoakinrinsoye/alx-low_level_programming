7c55a1dd9937f28d945578d40498e192908dcb3d
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-22 15:41:14 +0000

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * main - largest prime factror of 612852475143
+ *
+ * Return: 0
+ *
+ */
+int main (void)
+{
+	long int x, y, prime;
+
+	prime = -1;
+	x = 612852475143;
+
+	while (x % 2 == 0)
+	{
+		prime = 2;
+		x= x / 2;
+	}
+	for (y = 3; y <= x / 2; y = i + 2)
+	{
+		while (x % y == 0)
+		{
+			prime = y
+			x = x / y
+		}
+	}
+	if (x > 2)
+		prime = x;
+	printf("%ld\n", prime)
+	return (0);
+}
+
