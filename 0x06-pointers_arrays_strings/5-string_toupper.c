0f9f8339bb4fa161318441fcebd7925abb2dc710
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-25 23:16:01 +0000

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_toupper - function that changes all lowercase of a string to upper
+ * @str: string to convert
+ * Return: char
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
