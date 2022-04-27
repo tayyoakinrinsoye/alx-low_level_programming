cc6ed6181e00ac5c6330e4fd0019f6b0fb012af2
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-04-27 05:41:38 +0000

    Taskm1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memcopy - function copies n bytes from memory area src to dest
+ * @dest: memory location to be copied to
+ * @src: memory area to be copied from
+ * @n: number of bytes to copy
+ * Return: returns poointer to dest
+ */
+char *_memcopy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
