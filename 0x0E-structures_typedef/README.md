06482554d39e321c4961736379eaefdba3a885a1
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-05-11 22:27:32 +0000

    Structure declaration for dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Project on structure and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - data type containing dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Description: This structure is for dogs with the attributes
+ * name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
