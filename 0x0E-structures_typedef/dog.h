fac874f121f6e55b5ffc1e653f51de3d19c8a59e
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-05-11 23:24:11 +0000

    Updated dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,7 +26,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-og_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
