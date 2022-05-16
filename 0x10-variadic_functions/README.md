533670d954ba24a2b86d9b14517cd7e95dbcb0ed
tayyoakinrinsoye <tayoakinrinsoye@hotmail.com>
2022-05-16 19:20:18 +0000

    Task 0

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Project on variadic function
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _VARIADIC_
+#define _VARIADIC_
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
